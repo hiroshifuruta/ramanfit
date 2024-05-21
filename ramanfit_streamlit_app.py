2de4e960bb5ebaef0e3a2bbd43c991b49b52c07c
Hiroshi Furuta <hiroshifuruta@gmail.com>
2024-05-21 12:24:12 +0900

    modified:   ramanfit_streamlit_app.py

--- ramanfit_streamlit_app.py
+++ ramanfit_streamlit_app.py
@@ -104,9 +104,9 @@ if st.button("Analyze"):
     lorentz3 = LorentzianModel(prefix='l3_') # G' peak
     pars.update(lorentz3.make_params())
     pars['l3_center'].set(value=1615, min=1604, max=1630)
-    pars['l3_sigma'].set(value=16, min=2, max=120)
+    pars['l3_sigma'].set(value=16, min=5, max=120)
 #    pars['l3_amplitude'].set(value=30, min=2, max=4000)
-    l3_amplitude_min = 0.025 * pars['l2_amplitude'].value
+    l3_amplitude_min = 0.05 * pars['l2_amplitude'].value
     l3_amplitude_max = 0.1 * pars['l2_amplitude'].value
     pars['l3_amplitude'].set(value=30, min=l3_amplitude_min, max=l3_amplitude_max)
 
